cbe1ab25ffff25f65d3fd026fed1ea6d97eaa4b7
Tonio Serna <antonelo@gmail.com>
2021-05-17 11:38:22 +0200

    Add Mentor model and name, time_zone attributes

---
+++ db/migrate/20210517093555_create_mentors.rb
@@ -0,0 +1,10 @@
+class CreateMentors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :mentors do |t|
+      t.string :name
+      t.string :time_zone
+
+      t.timestamps
+    end
+  end
+end
